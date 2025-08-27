34fd24f18789b18e46734f77892346139343598b
Rob Stewart <robstewart57@gmail.com>
2025-08-27 18:29:29 +0100

    Splits the applyOp tests into separate test methods

--- test/CalculatorTest.java
+++ test/CalculatorTest.java
@@ -32,10 +32,41 @@ public class CalculatorTest {
 	}
 
 	@Test
-	public void testApplyOp() {
-		assertEquals(3,Calculator.applyOp("1","+","2"));
-		assertEquals(-5,Calculator.applyOp("2","-","7"));
-		assertEquals(6,Calculator.applyOp("3","+","3"));
-		assertEquals(3,Calculator.applyOp("5","-","2"));
+	public void testApplyOpPlus() {
+		assertEquals(3,Calculator.applyOp(1,"+",2));
+		assertEquals(0,Calculator.applyOp(3,"+",-3));
+		assertEquals(-3,Calculator.applyOp(0,"+",-3));
+		assertEquals(3,Calculator.applyOp(3,"+", 0));
+	}
+	
+	@Test
+	public void testApplyOpMinus() {
+		assertEquals(-5,Calculator.applyOp(2,"-",7));
+		assertEquals(3,Calculator.applyOp(5,"-",2));
+		assertEquals(7,Calculator.applyOp(5,"-",-2));
+		assertEquals(5,Calculator.applyOp(5,"-",0));
+		assertEquals(-5,Calculator.applyOp(0,"-",5));
+		assertEquals(5,Calculator.applyOp(0,"-",-5));
+	}
+	
+	@Test
+	public void testApplyOpTimes() {
+		assertEquals(6,Calculator.applyOp(2,"*",3));
+		assertEquals(-12,Calculator.applyOp(3,"*",-4));
+		assertEquals(0,Calculator.applyOp(3,"*",0));
+		assertEquals(0,Calculator.applyOp(0,"*",3));
+	}
+	
+	@Test
+	public void testApplyOpDivide() {
+		assertEquals(2,Calculator.applyOp(4,"/",2));
+		assertEquals(3,Calculator.applyOp(12,"/",4));
+		assertEquals(-3,Calculator.applyOp(12,"/",-4));
+		assertEquals(-3,Calculator.applyOp(-12,"/",4));
+	}
+	
+	@Test(expected=CalculateException.class)
+	public void testApplyOpException() {
+		assertEquals(2,Calculator.applyOp(4,"a",2));
 	}
 }
